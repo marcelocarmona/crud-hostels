cd2db6f21bfc6cf59f6beea342b45fc1bf184917
marcelocarmona <marcelo@marcelocarmona.com>
2017-02-08 23:39:32 -0300

    create page

--- client/app/common/common.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import hostelService from './hostel.service';
-
-const commonModule = angular.module('app.common', [])
-.service('hostelService', hostelService)
-.name;
-
-export default commonModule;
--- client/app/components/home/home.controller.js
+++
@@ -1,8 +0,0 @@
-class HomeController {
-  constructor(hostelService, $state) {
-    'ngInject';
-    this.hostelService = hostelService;
-  }
-}
-
-export default HomeController;
